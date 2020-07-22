9820ca2a5642df677b7f99ce083357f183c764d2
streetexnine <dmatius8@gmail.com>
2020-07-22 10:01:46 -0700

    David Project

---
+++ src/BMIprojek/BMI.java
@@ -0,0 +1,18 @@
+package BMIprojek;
+
+/**
+ *
+ * @author asus
+ */
+public class BMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView app = new LoginView();
+        app.show();
+    }
+    
+}
